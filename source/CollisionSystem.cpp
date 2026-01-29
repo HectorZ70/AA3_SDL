99d4417b0946531142f313c1c59be2fe985ef038
HectorZ70 <hector.zornoza@enti.cat>
2026-01-29 12:24:52 +0100

    CollisionSystem Fully Works

--- source/CollisionSystem.cpp
+++ source/CollisionSystem.cpp
@@ -3,27 +3,26 @@
 
 void CollisionSystem::Update(std::vector<Object*>& objects)
 {
-    const size_t size = objects.size();
-
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < objects.size(); i++)
 	{
-		for (int j = i + 1; j < objects.size(); j++)
+		for (size_t j = i + 1; j < objects.size(); j++)
 		{
 			if ((objects[i]->GetType() == ObjectType::BULLET && objects[j]->GetType() == ObjectType::BULLET)
 				|| (objects[i]->GetType() == ObjectType::ENEMY && objects[j]->GetType() == ObjectType::ENEMY)
 				|| (objects[i]->GetType() == ObjectType::PLAYER && objects[j]->GetType() == ObjectType::BULLET)
 				|| (objects[i]->GetType() == ObjectType::BULLET && objects[j]->GetType() == ObjectType::PLAYER))
 				continue;
+			if (objects[i]->IsPendingDestroy() || objects[j]->IsPendingDestroy())
+				continue;
 
 			if (objects[i]->GetRigidBody()->CheckCollision(objects[j]->GetRigidBody()))
 			{
 					std::cout << "COLLISION\n";
-					objects[i]->OnCollision(objects[j]);
-					objects[j]->OnCollision(objects[i]);
+					objects[i]->Destroy();
+					objects[j]->Destroy();
 			}
 		}
 	}
-
 	for (int i = objects.size() - 1; i >= 0; i--)
 	{
 		if (objects[i]->IsPendingDestroy())
--- source/Scene.h
+++ source/Scene.h
@@ -50,57 +50,6 @@ public:
 			u->Update();
 
 		_collisionSystem.Update(_objects);
-
-		/*
-		int size = _objects.size();
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = i + 1; j < _objects.size(); j++)
-			{
-				if ((_objects[i]->GetType() == ObjectType::BULLET && _objects[j]->GetType() == ObjectType::BULLET)
-					|| (_objects[i]->GetType() == ObjectType::ENEMY && _objects[j]->GetType() == ObjectType::ENEMY))
-					continue;
-
-				if (_objects[i]->GetRigidBody()->CheckCollision(_objects[j]->GetRigidBody()))
-				{
-					std::cout << "COLLISION\n";
-					_objects[i]->Destroy();
-					_objects[j]->Destroy();
-				}
-			}
-		}
-
-		size = _ui.size();
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = i + 1; j < _ui.size(); j++)
-			{
-				if (_ui[i]->GetRigidBody()->CheckCollision(_ui[j]->GetRigidBody()))
-				{
-					_ui[i]->Destroy();
-					_ui[j]->Destroy();
-				}
-			}
-		}
-
-		for (int i = _ui.size() - 1; i >= 0; i--)
-		{
-			if (_ui[i]->IsPendingDestroy())
-			{
-				delete _ui[i];
-				_ui.erase(_ui.begin() + i);
-			}
-		}
-
-		for (int i = _objects.size() - 1; i >= 0; i--)
-		{
-			if (_objects[i]->IsPendingDestroy())
-			{
-				delete _objects[i];
-				_objects.erase(_objects.begin() + i);
-			}
-		}
-		*/
 	}
 
 	virtual void Render()
